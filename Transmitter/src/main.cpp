99f8c390ca63ea90d4bf969aa0335f46a64c0425
Shaun Ruddy <shaun@arcinc.com>
2026-02-14 15:49:24 -0500

    receiver and transceiver updated code

--- Transmitter/src/main.cpp
+++ Transmitter/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//*This is the transmitter code
+#include <SPI.h>                  /* to handle the communication interface with the modem*/
+#include <nRF24L01.h>             /* to handle this particular modem driver*/
+#include <RF24.h>                 /* the library which helps us to control the radio modem*/
+
+RF24 radio(7, 8);                    /* Creating instance 'radio'  ( CE , CSN )   CE -> D7 | CSN -> D8 */                              
+const byte Address[6] = "00009";     /* Address to which data to be transmitted*/
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);              /* Start Serial communication */
+  radio.begin();                   /* Activate the modem */
+  radio.openWritingPipe(Address);  /* Set the address of the receiver */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Send the "Hello World" message repeatedly
+  radio.stopListening();            /* Set modem in transmission mode */
+  
+  const char message[] = "Hello World";   /* The message you want to send */
+  radio.write(&message, sizeof(message)); /* Send the message over NRF24L01 */
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Transmitting: ");
+  Serial.println(message);           /* Print the message on serial monitor */
+  
+  delay(1000);  /* Wait for 1 second before sending again */
 }
